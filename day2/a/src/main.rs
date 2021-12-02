8590f7aa9d03b3820771a6386c021b866563641a
olafsm <32181621+olafsm@users.noreply.github.com>
2021-12-02 07:21:33 +0100

    Day2 completed

---
+++ day2/a/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let input:Vec<&str> = include_str!("../input.txt")
+        .lines()
+        .collect();
+
+    let mut hor = 0;
+    let mut depth = 0;
+
+    for line in input {
+        let num = &line[line.len()-1..].trim().parse::<i32>().expect("Please type a number!");
+        match line[..line.len() - 2].as_ref() {
+            "forward"   => hor += num,
+            "up"        => depth -= num,
+            "down"      => depth += num,
+            _ =>{}
+        }
+    }
+    println!("Hor: {}, \nDepth: {}", hor, depth);
+    println!("Product: {}", hor * depth);
+}
