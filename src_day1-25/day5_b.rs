e8fa7d0c665d762931519e38e3646d1fa10db741
olafsm <32181621+olafsm@users.noreply.github.com>
2021-12-06 20:58:52 +0100

    Day 6 completed & Day 5 refactored

--- src_day1-25/day5_b.rs
+++ src_day1-25/day5_b.rs
@@ -18,6 +18,7 @@ fn main() {
     for line in input {
         let q:Vec<&str> = 
             line.split(" -> ").collect();
+            
         let point1:Vec<i32> = q[0].split(",").map(|i| i.parse::<i32>().unwrap()).collect(); // x1, y1
         let point2:Vec<i32> = q[1].split(",").map(|i| i.parse::<i32>().unwrap()).collect(); // x2, y2
         
@@ -26,11 +27,9 @@ fn main() {
 
         let y1 = point1[1];
         let y2 = point2[1];
-        if true == false {
 
-        }
-        // 7,0 -> 7,4
-        else if (x1 == x2) && (y1 != y2) {
+        // 8,0 -> 8,1
+        if (x1 == x2) && (y1 != y2) {
             if y1<=y2 {
                 for i in y1..y2+1 {
                     points.push(Point{x:x1,y:i})
@@ -41,7 +40,7 @@ fn main() {
                 }
             }
         } 
-        // 0,7 -> 4,7
+        // 0,8 -> 1,8
         else if (y1 == y2) && (x1 != x2) {
             if x1<=x2 {
                 for i in x1..x2+1 {
@@ -53,59 +52,48 @@ fn main() {
                 }
             }
         } 
-        // 1,1 -> 2,2
-        else if (y1 == x1) && (y2 == x2) {
-            if y1<y2 {
-                for i in y1..y2+1 {
-                    points.push(Point{x:i,y:i});
-                }
-            } else {
-                for i in y2..y1+1 {
-                    points.push(Point{x:i,y:i});
-                }
-            }
-        } 
-        // 8,0 -> 0,8
-        else if ((x1-x2)/(y1-y2)).abs() == 1 {
-
-            if x1 < x2 {
+        else {
+            if x1<x2 {
+                // 0,0  8,8
                 if y1<y2 {
                     let mut y = y1;
-                    for i in x1..x2+1 {
-                        points.push(Point{x:i,y:y});
-                        y += 1;
-                    }    
-                } else {
-                    let mut y = y1;
-                    for i in x1..x2+1 {
-                        points.push(Point{x:i,y:y});
-                        y -= 1;
-                    }    
+                    for x in x1..x2+1 {
+                        points.push(Point{x,y});
+                        y+=1;
+                    }
                 }
-            
-            } else {
-                let mut x = x1;
+                // 0,8 8,0
+                else {
+                    let mut y = y1;
+                    for x in x1..x2+1 {
+                        points.push(Point{x,y});
+                        y-=1;
+                    }
+                }
+            } 
+            else if x1>x2 {
+                // 8,0  0,8
                 if y1<y2 {
-                    for i in y1..y2+1 {
-                        points.push(Point{x:x,y:i});
+                    let mut x = x1;
+                    for y in y1..y2+1 {
+                        points.push(Point{x,y});
                         x-=1;
                     }
-                }else {
+                }
+                // 8,8  0,0
+                else {
                     let mut y = y1;
                     let mut x = x1;
-                    while y != y2 {
-                        points.push(Point{x:x,y:y});
+                    while y != y2-1 {
+                        points.push(Point{x,y});
                         y-=1;
                         x-=1;
                     }
-               }
+                }
             }
         }
     }
     let mut hashes = HashMap::new();
-    //for point in points.clone() {
-    //    println!("{}", point);
-    //}
     for point in points {
         if hashes.contains_key(&point) {
             *hashes.get_mut(&point).unwrap()+=1;
@@ -113,10 +101,6 @@ fn main() {
         }
         hashes.insert(point, 1);
     }
-    //for (k, v) in hashes.iter()
-    //    .filter(|i| i.1 >= &2) {
-    //        println!("{} Value: {}", k, v)
-    //    }
     let res = hashes.iter().filter(|i| i.1 >= &2).count();
     println!("{}", res);
 }
---
+++ src_day1-25/day6_b.rs
@@ -0,0 +1,25 @@
+/*
+* Advent of Code 2021
+* Day 6b
+* @olafsm  
+*/
+
+fn main() {
+    let mut input:Vec<usize> = include_str!("../input.txt")
+        .split(",")
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect();
+
+    let mut numbers = [0;9];
+    for num in input {
+        numbers[num] += 1;
+    }
+
+    for i in 1..257 {
+        let first = numbers[0];
+        numbers.rotate_left(1);
+        numbers[6] += first;
+    }
+    let total:usize = numbers.iter().sum();
+    println!("{}", total);
+}
