a2b7d5128848da10e1db350622dac5ece20e1d4a
olafsm <32181621+olafsm@users.noreply.github.com>
2021-12-08 14:41:31 +0100

    Refactor day8

--- src_day1-25/day8_b.rs
+++ src_day1-25/day8_b.rs
@@ -33,14 +33,15 @@ fn get_output_value(mut input:Vec<String>, output:Vec<String>)  -> i32 {
 
     let mut characters = HashMap::new();
     
+    // 1, 4, 7 and 8 are unique by length
     characters.insert(input[0].to_string(), "1");
     characters.insert(input[1].to_string(), "7");
     characters.insert(input[2].to_string(), "4");
     characters.insert(input[9].to_string(), "8");
     
-    // Find 0 and 9 by using 4
+    // Find 0, 6 and 9
     for x in &input[6..9] {
-        // Find 0 or 9 
+
         let mut count = 0;
         for letter in x.chars() {
             if !input[2].contains(letter) {
@@ -53,40 +54,31 @@ fn get_output_value(mut input:Vec<String>, output:Vec<String>)  -> i32 {
             _ => {},
         };
 
-        // find 6
-        for letter in input[0].chars() {
-            if !x.contains(letter) {
-                characters.insert(x.to_string(), "6");
-            }
+        if input[0].chars().any(|letter| !x.contains(letter)) {
+            characters.insert(x.to_string(), "6");
         }
     }
 
     // find 2, 3 & 5
     for x in &input[3..6] {
-        // find 3
-        let mut condition = true;
-        for letter in input[0].chars() {
-            if !x.contains(letter) {
-                condition = false;
+        let mut count = 0;
+        for letter in x.chars() {
+            if !input[2].contains(letter){
+                count +=1;
             }
         }
-        if condition {
+        match count {
+            2 => { characters.insert(x.to_string(), "5"); },
+            3 => { characters.insert(x.to_string(), "2"); },
+            _ => {},
+        };
+
+        if input[0].chars().all(|letter| x.contains(letter)) {
             characters.insert(x.to_string(), "3");
-        } else {
-            let mut count = 0;
-            for letter in x.chars() {
-                if !input[2].contains(letter){
-                    count +=1;
-                }
-            }
-            match count {
-                2 => { characters.insert(x.to_string(), "5"); },
-                3 => { characters.insert(x.to_string(), "2"); },
-                _ => {},
-            };
-        }
+        } 
     }
 
+    // Get values from HashMap and return integer
     let mut completed_string = "".to_string();
     for x in output {
         completed_string += characters.get(&x).unwrap();
